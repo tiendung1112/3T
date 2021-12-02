ad5fc6f156700e195e197aa0053da64984669695
tiendung1112 <80438011+tiendung1112@users.noreply.github.com>
2021-12-02 12:50:31 +0700

    fix bug

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const db = require('./config/db');
 db.connect();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT ||5000;
 
 app.use(express.json());
 app.use(express.urlencoded());
