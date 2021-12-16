d3cff2bb615e85f208523c7b560cb8e728075200
tiendung1112 <80438011+tiendung1112@users.noreply.github.com>
2021-12-17 05:17:40 +0700

    fix play

--- src/public/css/app.js
+++ src/public/css/app.js
@@ -12,6 +12,44 @@ fetch('http://localhost:4000/musics/get')
             } 
         }) 
         console.log(songs);
+        const $ = document.querySelector.bind(document);
+        const $$ = document.querySelectorAll.bind(document);
+        const playlistitem = $$(".playlist-item");
+        const playlist =$$(".playlist");
+
+            const app = {
+            handleEvents: function() {
+                for(var i=0; i<playlistitem.length; i++) {
+                playlistitem[i].onclick = function() {
+                    var tmp = this.querySelector("a").outerText;
+                    console.log(tmp);
+                    console.log(this);
+                    for (var j = 0; j<songs.length; j++) {
+                        playlistitem[j].style.backgroundColor = "#ffffff";
+                        if(songs[j].name== tmp) {
+                            var indextmp = j;
+                            playlistitem[indextmp].style.backgroundColor = "#0000004d";
+                        }
+                    };
+                
+                    const ap = new APlayer({
+                        container: document.getElementById('aplayer'),
+                        audio: [{
+                            name: songs[indextmp].name,
+                            artist: songs[indextmp].artist,
+                            url: 'http://localhost:4000/'+songs[indextmp].url,
+                            cover: 'https://picsum.photos/200'
+                        }]
+                    });
+                    ap.play();
+                };
+                };
+            },  
+            start: function() {
+                this.handleEvents();
+            },
+        }
+    app.start();
     });
 
 
