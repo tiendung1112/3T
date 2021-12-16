96873562587cffecfbf9cfc6c28de8252f077517
Zauuuu 1310 <jaykate00@gmail.com>
2021-12-16 19:11:29 +0700

    phatnhac

--- src/public/css/app.js
+++ src/public/css/app.js
@@ -1,3 +1,16 @@
+var songs;
+
 fetch('http://localhost:4000/musics/get')
     .then(response => response.json())
-    .then(musics => console.log(musics))
+    .then(musics => {
+        songs = musics.map(function(music, index) {
+            return {
+                name: music.name,
+                artist: music.singer,
+                url: music.audio,
+                cover: ""
+            } 
+        }) 
+        console.log(songs);
+    });
+ 
